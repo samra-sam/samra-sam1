cfb4c0f8e53fde16a8846a071accfbd4e06bf5bf
samra <samraabdulhameed7@gmail.com>
2024-09-22 21:12:53 +0500

    This is a nextjs project

---
+++ src/app/age/birthmonth/page.tsx
@@ -0,0 +1,8 @@
+export default function BirthMonth(){
+    return(
+        <div>
+            <h3>Birth Month</h3>
+            <p>My Birth month is <b>December</b></p>
+        </div>
+    )
+}
---
+++ src/app/age/page.tsx
@@ -0,0 +1,8 @@
+export default function age(){
+    return(
+        <div>
+            <h2>Age</h2>
+            <p>I am 25 years old</p>
+        </div>
+    )
+}
